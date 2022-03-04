97170a13c800fa1a03692179d6eaa31be7e6b993
Chandana Patnala <cpatnala@vmware.com>
2022-03-04 14:28:49 +0530

    Update ansible_utils.py

--- plugins/module_utils/utils/ansible_utils.py
+++ plugins/module_utils/utils/ansible_utils.py
@@ -351,7 +351,7 @@ def get_api_context(module, api_creds):
         return None
 
 
-NO_UUID_OBJ = ['cluster', 'systemconfiguration']
+NO_UUID_OBJ = ['cluster', 'systemconfiguration', 'inventoryfaultconfig']
 SKIP_DELETE_ERROR = ["Cannot delete system default object", "Method \'DELETE\' not allowed"]
 
 
