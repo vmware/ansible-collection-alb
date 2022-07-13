1925a42b4e266b221cd0b15905cdc8cf350aa90c
Yograj Shisode <yshisode@vmware.com>
2022-07-13 01:52:25 -0700

    Updated assets for ansible collection
    Signed-off-by: Yograj Shisode <yshisode@vmware.com>

--- plugins/modules/avi_pulse_registration.py
+++ plugins/modules/avi_pulse_registration.py
@@ -185,11 +185,12 @@ obj:
 '''
 
 import os
+import time
 from ansible.module_utils.basic import AnsibleModule
 
 try:
     from ansible_collections.vmware.alb.plugins.module_utils.utils.ansible_utils import (
-        avi_common_argument_spec,AviCheckModeResponse, ansible_return, avi_obj_cmp,
+        avi_common_argument_spec, AviCheckModeResponse, ansible_return, avi_obj_cmp,
         cleanup_absent_fields)
     from ansible_collections.vmware.alb.plugins.module_utils.avi_api import (
         ApiSession, AviCredentials)
@@ -199,186 +200,188 @@ except ImportError:
 
 
 def main():
-    case_spec=dict(
-        enable_auto_case_creation_on_controller_failure=dict(default='False',choices=['True', 'False']),
-        enable_auto_case_creation_on_se_failure=dict(default='False',choices=['True', 'False'])
+    case_spec = dict(
+        enable_auto_case_creation_on_controller_failure=dict(default='False', choices=['True', 'False']),
+        enable_auto_case_creation_on_se_failure=dict(default='False', choices=['True', 'False'])
     )
-    waf_spec=dict(
-        enable_auto_download_waf_signatures=dict(default='False',choices=['True', 'False']),
-        enable_waf_signatures_notifications=dict(default='False',choices=['True', 'False'])
+    waf_spec = dict(
+        enable_auto_download_waf_signatures=dict(default='False', choices=['True', 'False']),
+        enable_waf_signatures_notifications=dict(default='False', choices=['True', 'False'])
 
     )
     argument_specs = dict(
-        state=dict(default='present',choices=['absent', 'present']),
+        state=dict(default='present', choices=['absent', 'present']),
         jwt_token=dict(type='str', required=True,no_log=True),
-        name=dict(required=True,type='str'),
+        name=dict(required=True, type='str'),
         params=dict(type='dict'),
         description=dict(type='str', required=True),
         email=dict(type='str', required=True),
-        account_id=dict(type='str',required=True),
-        optins=dict(default='present',choices=['absent', 'present']),
-        enable_cleanup_of_attached_files=dict(default='False',choices=['True', 'False']),
-        enable_appsignature_sync=dict(default='False',choices=['True', 'False']),
-        enable_ip_reputation=dict(default='False',choices=['True', 'False']),
-        enable_pulse_case_management=dict(default='False',choices=['True', 'False']),
-        enable_pulse_waf_management=dict(default='False',choices=['True', 'False']),
-        enable_user_agent_db_sync=dict(default='False',choices=['True', 'False']),
-        use_tls=dict(default='False',choices=['True', 'False']),
-        waf_config=dict(type='dict',options=waf_spec),
-        case_config=dict(type='dict',options=case_spec)
+        account_id=dict(type='str', required=True),
+        optins=dict(default='present', choices=['absent', 'present']),
+        enable_cleanup_of_attached_files=dict(default='False', choices=['True', 'False']),
+        enable_appsignature_sync=dict(default='False', choices=['True', 'False']),
+        enable_ip_reputation=dict(default='False', choices=['True', 'False']),
+        enable_pulse_case_management=dict(default='False', choices=['True', 'False']),
+        enable_pulse_waf_management=dict(default='False', choices=['True', 'False']),
+        enable_user_agent_db_sync=dict(default='False', choices=['True', 'False']),
+        use_tls=dict(default='False', choices=['True', 'False']),
+        waf_config=dict(type='dict', options=waf_spec),
+        case_config=dict(type='dict', options=case_spec)
     )
     argument_specs.update(avi_common_argument_spec())
-    module = AnsibleModule(argument_spec=argument_specs,supports_check_mode=True)
+    module = AnsibleModule(argument_spec=argument_specs, supports_check_mode=True)
     if not HAS_REQUESTS:
         return module.fail_json(msg=(
             'Python requests package is not installed. '
             'For installation instructions, visit https://pypi.org/project/requests.'))
-    
 
-    api_creds = AviCredentials()                                                                                                                    
+    api_creds = AviCredentials()
     api_creds.update_from_ansible_module(module)
-    api = ApiSession.get_session(api_creds.controller, api_creds.username, password=api_creds.password,api_version=api_creds.api_version,timeout=api_creds.timeout,          tenant=api_creds.tenant,
-        tenant_uuid=api_creds.tenant_uuid, token=api_creds.token,
-        port=api_creds.port)
+    api = ApiSession.get_session(
+        api_creds.controller, api_creds.username, password=api_creds.password,
+        api_version=api_creds.api_version, timeout=api_creds.timeout,
+        tenant=api_creds.tenant, tenant_uuid=api_creds.tenant_uuid,
+        token=api_creds.token, port=api_creds.port)
     api_version = api_creds.api_version
     tenant_uuid = api_creds.tenant_uuid
     tenant = api_creds.tenant
     check_mode = module.check_mode
     path = module.params.get('path', '')
-    state= module.params.get('state', None)
+    state = module.params.get('state', None)
     jwt_token = module.params.get('jwt_token', None)
-    name= module.params.get('name', None)
-    description=module.params.get('description', None)
+    name = module.params.get('name', None)
+    description = module.params.get('description', None)
     email = module.params.get('email', None)
     account_id = module.params.get('account_id', None)
-    portal_url= 'https://portal.avipulse.vmware.com'
-    optins=module.params.get('optins',None)
-    enable_cleanup_of_attached_files=module.params.get('enable_cleanup_of_attached_files', None)
-    enable_appsignature_sync=module.params.get('enable_appsignature_sync', None)
-    enable_ip_reputation=module.params.get('enable_ip_reputation', None)
-    enable_pulse_case_management=module.params.get('enable_pulse_case_management', None)
-    enable_pulse_waf_management=module.params.get('enable_pulse_waf_management', None)
-    enable_user_agent_db_sync=module.params.get('enable_user_agent_db_sync', None)
-    use_tls=module.params.get('use_tls', None)                                                                                                      
-    waf_config=module.params.get('waf_config',None)
-    case_config=module.params.get('case_config',None)
+    portal_url = 'https://portal.avipulse.vmware.com'
+    optins = module.params.get('optins', None)
+    enable_cleanup_of_attached_files = module.params.get('enable_cleanup_of_attached_files', None)
+    enable_appsignature_sync = module.params.get('enable_appsignature_sync', None)
+    enable_ip_reputation = module.params.get('enable_ip_reputation', None)
+    enable_pulse_case_management = module.params.get('enable_pulse_case_management', None)
+    enable_pulse_waf_management = module.params.get('enable_pulse_waf_management', None)
+    enable_user_agent_db_sync = module.params.get('enable_user_agent_db_sync', None)
+    use_tls = module.params.get('use_tls', None)
+    waf_config = module.params.get('waf_config', None)
+    case_config = module.params.get('case_config', None)
     enable_auto_download_waf_signatures = module.params.get('waf_config')['enable_auto_download_waf_signatures']
     enable_waf_signatures_notifications = module.params.get('waf_config')['enable_waf_signatures_notifications']
-    enable_auto_case_creation_on_controller_failure= module.params.get('case_config')['enable_auto_case_creation_on_controller_failure']
-    enable_auto_case_creation_on_se_failure=module.params.get('case_config')['enable_auto_case_creation_on_se_failure']
-    reg=True
-    
+    enable_auto_case_creation_on_controller_failure = module.params.get('case_config')['enable_auto_case_creation_on_controller_failure']
+    enable_auto_case_creation_on_se_failure = module.params.get('case_config')['enable_auto_case_creation_on_se_failure']
+    reg = True
+
     if state == 'present':
         #registration
-        path="albservices/status"
-        resp=api.get(path,tenant=tenant, tenant_uuid=tenant_uuid,api_version=api_version)
-        existing_obj=resp
+        path = "albservices/status"
+        resp = api.get(path, tenant=tenant, tenant_uuid=tenant_uuid, api_version=api_version)
+        existing_obj = resp
         if not(check_mode) and resp.json().get("connectivity_status") == "ALBSERVICES_DISCONNECTED":
-            headers = {'Content-Type': 'application/json','Authorization':'Basic YWRtaW46YWRtaW4='}
-            data={"jwt_token":jwt_token}
-            path="portal/refresh-access-token"
-            rsp = api.post(path, api_version=api_version,headers=headers,data=data)
-            if rsp.status_code>300:
+            headers = {'Content-Type': 'application/json', 'Authorization':'Basic YWRtaW46YWRtaW4='}
+            data = {"jwt_token":jwt_token}
+            path = "portal/refresh-access-token"
+            rsp = api.post(path, api_version=api_version, headers=headers, data=data)
+            if rsp.status_code > 300:
                 return module.fail_json(msg='Failed: %s' %rsp.text)
             else:
-                changed=True
+                changed = True
                 time.sleep(10)
-          
 
         if resp.json().get("registration_status") == "ALBSERVICES_DEREGISTERED":
             if not check_mode:
-                path="albservices/register"
-                data= {"name":name,"description":description,"email":email,"account_id":account_id}
+                path = "albservices/register"
+                data = {"name":name, "description":description, "email":email,
+                       "account_id":account_id}
                 rsp = api.post(path, data=data)
-                changed=True
+                changed = True
                 time.sleep(5)
             else:
                     # No need to process any further.
                     rsp = AviCheckModeResponse(obj=existing_obj)
-                    changed=True
+                    changed = True
 
         if resp.json().get("registration_status") == "ALBSERVICES_REGISTERED":
-            changed=False
-            reg=False
+            changed = False
+            reg = False
 
-        if reg and rsp.status_code>300:
+        if reg and rsp.status_code > 300:
             return module.fail_json(msg='Failed: %s' %rsp.text)
         else:
             if  optins == 'present':
-                if enable_pulse_case_management=='False' and (enable_auto_case_creation_on_se_failure or 
-                enable_auto_case_creation_on_controller_failure):                                                                                                                   
+                if enable_pulse_case_management == 'False' and (enable_auto_case_creation_on_se_failure or
+                enable_auto_case_creation_on_controller_failure):
                     return module.fail_json(msg='Unable to enable the options as enable_pulse_case_management is not enabled.')
-            
-                if enable_pulse_waf_management=='False' and (enable_auto_download_waf_signatures or enable_waf_signatures_notifications):
+
+                if enable_pulse_waf_management == 'False' and (enable_auto_download_waf_signatures or enable_waf_signatures_notifications):
                     return module.fail_json(msg='Unable to enable the options as enable_pulse_waf_management is not enabled.')
-                path="albservicesconfig"
-                response=api.get(path,tenant=tenant, tenant_uuid=tenant_uuid,api_version=api_version)
-                path= "albservicesconfig"
+                path = "albservicesconfig"
+                response = api.get(path, tenant=tenant, tenant_uuid=tenant_uuid,
+                                   api_version=api_version)
+                path = "albservicesconfig"
                 data = dict()
-                data["portal_url"]= portal_url
-                data["polling_interval"]= 10
-                data["feature_opt_in_status"]= dict()
-                data[("feature_opt_in_status")]["enable_appsignature_sync"]=enable_appsignature_sync
-                data[("feature_opt_in_status")]["enable_ip_reputation"]=enable_ip_reputation
-                data[("feature_opt_in_status")]["enable_pulse_case_management"]= enable_pulse_case_management
-                data[("feature_opt_in_status")]["enable_pulse_waf_management"]=enable_pulse_waf_management
-                data[("feature_opt_in_status")]["enable_user_agent_db_sync"]= enable_user_agent_db_sync
-                data["ip_reputation_config"]= dict()
-                data[("ip_reputation_config")]["ip_reputation_file_object_expiry_duration"]=3
-                data[("ip_reputation_config")]["ip_reputation_sync_interval"]=60
-                data["use_tls"]=use_tls
-                data["mode"]="MYVMWARE"
-                data["app_signature_config"]=dict()
-                data[("app_signature_config")]["app_signature_sync_interval"]=1440
-                data["user_agent_db_config"]= dict()
-                data[("user_agent_db_config")]["allowed_batch_size"]=500
-                data["waf_config"]=dict ()                                                                                                                    
-                data[("waf_config")]["enable_auto_download_waf_signatures"]= enable_auto_download_waf_signatures
-                data[("waf_config")]["enable_waf_signatures_notifications"]= enable_waf_signatures_notifications
-                data["case_config"]=dict ()
-                data[("case_config")]["enable_auto_case_creation_on_controller_failure"]= enable_auto_case_creation_on_controller_failure
-                data[("case_config")]["enable_auto_case_creation_on_se_failure"]= enable_auto_case_creation_on_se_failure
-                data[("case_config")]["enable_cleanup_of_attached_files"]= enable_cleanup_of_attached_files
-                data["saas_licensing_config"]=dict()
-                data[("saas_licensing_config")]["max_service_units"]= 1000
-                data[("saas_licensing_config")]["reserve_service_units"]= 0
+                data["portal_url"] = portal_url
+                data["polling_interval"] = 10
+                data["feature_opt_in_status"] = dict()
+                data[("feature_opt_in_status")]["enable_appsignature_sync"] = enable_appsignature_sync
+                data[("feature_opt_in_status")]["enable_ip_reputation"] = enable_ip_reputation
+                data[("feature_opt_in_status")]["enable_pulse_case_management"] = enable_pulse_case_management
+                data[("feature_opt_in_status")]["enable_pulse_waf_management"] = enable_pulse_waf_management
+                data[("feature_opt_in_status")]["enable_user_agent_db_sync"] = enable_user_agent_db_sync
+                data["ip_reputation_config"] = dict()
+                data[("ip_reputation_config")]["ip_reputation_file_object_expiry_duration"] = 3
+                data[("ip_reputation_config")]["ip_reputation_sync_interval"] = 60
+                data["use_tls"] = use_tls
+                data["mode"] = "MYVMWARE"
+                data["app_signature_config"] = dict()
+                data[("app_signature_config")]["app_signature_sync_interval"] = 1440
+                data["user_agent_db_config"] = dict()
+                data[("user_agent_db_config")]["allowed_batch_size"] = 500
+                data["waf_config"] = dict()
+                data[("waf_config")]["enable_auto_download_waf_signatures"] = enable_auto_download_waf_signatures
+                data[("waf_config")]["enable_waf_signatures_notifications"] = enable_waf_signatures_notifications
+                data["case_config"] = dict()
+                data[("case_config")]["enable_auto_case_creation_on_controller_failure"] = enable_auto_case_creation_on_controller_failure
+                data[("case_config")]["enable_auto_case_creation_on_se_failure"] = enable_auto_case_creation_on_se_failure
+                data[("case_config")]["enable_cleanup_of_attached_files"] = enable_cleanup_of_attached_files
+                data["saas_licensing_config"] = dict()
+                data[("saas_licensing_config")]["max_service_units"] = 1000
+                data[("saas_licensing_config")]["reserve_service_units"] = 0
                 data_for_cmp = data
                 response_data = response.json()
-                ch = not avi_obj_cmp(data_for_cmp,response_data)
+                ch = not avi_obj_cmp(data_for_cmp, response_data)
                 if ch:
                     if not check_mode:
                         rsp = api.put(path, data=data)
-                        changed=True
+                        changed = True
                     else:                                                                                                                           
                         rsp = AviCheckModeResponse(obj=existing_obj)
-                        changed=True
-            return module.exit_json(changed=changed,msg='Registered successfully')
+                        changed = True
+            return module.exit_json(changed=changed, msg='Registered successfully')
 
     else:
         #deregistration
-        reg=True
-        path="albservices/status"
-        resp=api.get(path,tenant=tenant, tenant_uuid=tenant_uuid,api_version=api_version)
-        existing_obj=resp
-        if resp.json().get("registration_status")=="ALBSERVICES_REGISTERED":
+        reg = True
+        path = "albservices/status"
+        resp = api.get(path, tenant=tenant, tenant_uuid=tenant_uuid,
+                       api_version=api_version)
+        existing_obj = resp
+        if resp.json().get("registration_status") == "ALBSERVICES_REGISTERED":
             if not check_mode:
-                path="albservices/register"
-                data={"status":'Obsolete'}
+                path = "albservices/register"
+                data = {"status":'Obsolete'}
                 rsp = api.put(path, data=data)
-                changed=True
+                changed = True
             else:
                 rsp = AviCheckModeResponse(obj=existing_obj)
-                changed=True
-                
+                changed = True
+
         if resp.json().get("registration_status")=="ALBSERVICES_DEREGISTERED":
-            changed=False
-            reg=False
+            changed = False
+            reg = False
         if reg and rsp.status_code > 300:
-            return module.fail_json(msg='Failed: %s' %
-                                        rsp.text)
+            return module.fail_json(msg='Failed: %s' % rsp.text)
         else:
             return module.exit_json(
-                    changed=changed, msg="Deregistered successfully")  
-    
+                    changed=changed, msg="Deregistered successfully")
+
 if __name__ == '__main__':
-    main()                                     
+    main()
