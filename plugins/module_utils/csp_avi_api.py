e6d73a16ac7a1d80907708af786c39d59b097d45
Sagar <ssalvi@vmware.com>
2023-03-16 01:54:35 -0700

    Updated assets for ansible collection
    Signed-off-by: Sagar <ssalvi@vmware.com>

--- plugins/module_utils/csp_avi_api.py
+++ plugins/module_utils/csp_avi_api.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
 
-from avi.sdk.avi_api import ApiSession, \
+from ansible_collections.vmware.alb.plugins.module_utils.avi_api import ApiSession, \
     sessionDict, APIError, AviCredentials
 import os
 import sys
