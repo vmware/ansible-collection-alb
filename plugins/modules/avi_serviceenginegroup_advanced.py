8ddf5db606775b5abbd34bdc535e65abf19117d6
cpatnala <cpatnala@vmware.com>
2023-06-26 16:25:38 +0530

    Fixing sanity check failures

--- plugins/modules/avi_serviceenginegroup_advanced.py
+++ plugins/modules/avi_serviceenginegroup_advanced.py
@@ -715,7 +715,7 @@ obj:
 from ansible.module_utils.basic import AnsibleModule
 try:
     from ansible_collections.vmware.alb.plugins.module_utils.utils.ansible_utils import (
-        avi_common_argument_spec, avi_ansible_api, ansible_return)
+        avi_common_argument_spec, avi_ansible_api, ansible_return, purge_optional_fields, avi_obj_cmp, AviCheckModeResponse)
     from ansible_collections.vmware.alb.plugins.module_utils.avi_api import (
         ApiSession,
         AviCredentials,
