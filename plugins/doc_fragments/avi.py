a05fdde25ea56f455e5cbce7d108ff1d54ecf6fe
cpatnala <cpatnala@vmware.com>
2024-03-19 22:21:01 +0530

    Missing commits in 22.1.6

--- plugins/doc_fragments/avi.py
+++ plugins/doc_fragments/avi.py
@@ -40,7 +40,7 @@ options:
         description:
             - Avi API version of to use for Avi API and objects.
         type: str
-        default: 18.2.6
+        default: 20.1.1
     avi_credentials:
         description:
             - Avi Credentials dictionary which can be used in lieu of enumerating Avi Controller login details.
@@ -61,7 +61,7 @@ options:
                 description:
                   - Avi controller version
                 type: str
-                default: 18.2.6
+                default: 20.1.1
             tenant:
                 description:
                   - Avi controller tenant
@@ -92,6 +92,18 @@ options:
             csrftoken:
                 description:
                   - Avi controller API csrftoken to reuse existing session with session id
+                type: str
+            csp_host:
+                description:
+                  - CSP host for Saas controller
+                type: str
+            csp_token:
+                description:
+                  - Saas controller password
+                type: str
+            idp_class:
+                description:
+                  - Specifies whether it is an Avi controller or Saas controller
                 type: str
         type: dict
     api_context:
