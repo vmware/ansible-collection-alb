d9fbcee45e52e19bd5bef1da16047666710e584d
avijenkins <svc.avi-jenkins@broadcom.com>
2025-12-17 09:07:37 +0000

    Updated assets for ansible collection
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- plugins/module_utils/utils/ansible_utils.py
+++ plugins/module_utils/utils/ansible_utils.py
@@ -498,9 +498,10 @@ def avi_ansible_api(module, obj_type, sensitive_fields):
         # is actually in admin tenant.
         if existing_obj and 'tenant_ref' in obj and 'tenant_ref' in existing_obj:
             # https://10.10.25.42/api/tenant/admin#admin
-            if not obj.get('tenant_ref').startswith("/api/tenant/?name=") and obj.get('tenant_ref') is not None:
-                raise InvalidRefFormat(
-                    f"Invalid tenant_ref format: {obj['tenant_ref']}. Expected format: /api/tenant/?name=<name> (specifying the tenant name by name).")
+            # Commenting this check for this JIRA AV-252058. Added fix for 'tenant_ref' to accept with and without '/'. Removed the validation check.
+            # if not obj.get('tenant_ref').startswith("/api/tenant/?name=") and obj.get('tenant_ref') is not None:
+            #     raise InvalidRefFormat(
+            #         f"Invalid tenant_ref format: {obj['tenant_ref']}. Expected format: /api/tenant/?name=<name> (specifying the tenant name by name).")
             existing_obj_tenant = existing_obj['tenant_ref'].split('#')[1]
             obj_tenant = obj['tenant_ref'].split('name=')[1]
             if obj_tenant != existing_obj_tenant:
