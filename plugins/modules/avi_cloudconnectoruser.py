3ab1d42d4819ddac0d28c9ab37a1df2bb4acacde
avijenkins <svc.avi-jenkins@broadcom.com>
2026-04-15 09:01:08 +0000

    Updated assets for ansible collection
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- plugins/modules/avi_cloudconnectoruser.py
+++ plugins/modules/avi_cloudconnectoruser.py
@@ -217,7 +217,7 @@ def main():
             'Python requests package is not installed. '
             'For installation instructions, visit https://pypi.org/project/requests.'))
     return avi_ansible_api(module, 'cloudconnectoruser',
-                           {'obj_password', 'new_password_enc', 'private_key', 'password', 'azure_userpass'})
+                           {'new_password_enc', 'password', 'private_key', 'azure_userpass', 'obj_password'})
 
 
 if __name__ == '__main__':
