07cb69e7fe4b12f3e6bad1348e627d9e6bc8f9e0
avijenkins <svc.avi-jenkins@broadcom.com>
2026-05-01 09:07:27 +0000

    Updated assets for ansible collection
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- plugins/modules/avi_controllerproperties.py
+++ plugins/modules/avi_controllerproperties.py
@@ -1170,7 +1170,7 @@ def main():
             'Python requests package is not installed. '
             'For installation instructions, visit https://pypi.org/project/requests.'))
     return avi_ansible_api(module, 'controllerproperties',
-                           {'portal_token', 'cc_user_password_expiry_days', 'intelligent_assist_project_key'})
+                           {'intelligent_assist_project_key', 'portal_token', 'cc_user_password_expiry_days'})
 
 
 if __name__ == '__main__':
--- plugins/modules/avi_sslkeyandcertificate.py
+++ plugins/modules/avi_sslkeyandcertificate.py
@@ -331,7 +331,7 @@ def main():
             'Python requests package is not installed. '
             'For installation instructions, visit https://pypi.org/project/requests.'))
     return avi_ansible_api(module, 'sslkeyandcertificate',
-                           {'key_params', 'enckey_base64', 'key', 'key_passphrase'})
+                           {'key', 'key_params', 'key_passphrase', 'enckey_base64'})
 
 
 if __name__ == '__main__':
