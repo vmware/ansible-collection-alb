47af96d9a874b776322c3ef9cdae0595d26e11a7
Naresh Patil <naresh.patil@broadcom.com>
2026-05-22 09:41:48 +0000

    Fixed sanity issue

--- plugins/modules/avi_serviceenginegroup.py
+++ plugins/modules/avi_serviceenginegroup.py
@@ -1857,8 +1857,18 @@ def main():
         vs_se_scaleout_additional_wait_time=dict(type='int'),
         vs_se_scaleout_ready_timeout=dict(type='int'),
         vs_switchover_timeout=dict(type='int'),
+        api_context=dict(type='dict',),
+        username=dict(type='str', default=''),
+        tenant_uuid=dict(type='str', default=''),
+        tenant=dict(type='str', default='admin'),
+        password=dict(type='str', default='', no_log=True),
+        controller=dict(type='str', default=''),
+        api_version=dict(type='str', default='20.1.7'),
+        avi_credentials=dict(type='dict',),
+        avi_deactivate_session_cache_as_fact=dict(type='bool', default=False),
     )
-    argument_specs.update(avi_common_argument_spec())
+    if HAS_REQUESTS:
+        argument_specs.update(avi_common_argument_spec())
     module = AnsibleModule(argument_spec=argument_specs,
                            supports_check_mode=True)
     if not HAS_REQUESTS:
