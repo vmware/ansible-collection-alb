2a98b4df767946773d6a585d3d8128477bddd80d
Naresh Patil <naresh.patil@broadcom.com>
2026-05-28 06:54:20 +0000

    Reverted the linces change

--- tests/unit/modules/test_avi_pool.py
+++ tests/unit/modules/test_avi_pool.py
@@ -6,7 +6,7 @@
 ###
 
 # Copyright 2021 VMware, Inc.  All rights reserved. VMware Confidential
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: Apache License 2.0
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
