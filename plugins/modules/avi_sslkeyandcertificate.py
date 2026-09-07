92b5ca4c6946e0b783d82f3c6b3fccdb3e6585aa
avijenkins <svc.avi-jenkins@broadcom.com>
2026-09-07 09:02:53 +0000

    Updated assets for ansible collection
    Signed-off-by: avijenkins <svc.avi-jenkins@broadcom.com>

--- plugins/modules/avi_sslkeyandcertificate.py
+++ plugins/modules/avi_sslkeyandcertificate.py
@@ -333,7 +333,7 @@ def main():
             'Python requests package is not installed. '
             'For installation instructions, visit https://pypi.org/project/requests.'))
     return avi_ansible_api(module, 'sslkeyandcertificate',
-                           {'key', 'key_passphrase', 'key_params', 'enckey_base64'})
+                           {'key', 'enckey_base64', 'key_params', 'key_passphrase'})
 
 
 if __name__ == '__main__':
